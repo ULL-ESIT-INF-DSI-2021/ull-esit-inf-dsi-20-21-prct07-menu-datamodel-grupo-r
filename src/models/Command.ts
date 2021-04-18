21d469e5f29a87568bec826367a5a03453795b59
Hugo Fernández Solís <alu0101112664@ull.edu.es>
2021-04-18 20:16:00 +0100

    Añadida la clase commands

--- src/models/Command.ts
+++ src/models/Command.ts
@@ -1,4 +1,24 @@
-export class Command {
-  private id: number;
-  private command: string;
+/**
+ * Implementa los comandos disponibles en la aplicación.
+ * @var id_ Identificador del comando.
+ * @var command_ Texto del comando.
+ */
+export abstract class Command {
+  private id_: number;
+  private command_: string;
+
+  /**
+   * Inicializa los valores del comando.
+   * @param id Identificador del comando.
+   * @param command Texto del comando a ejecutar.
+   */
+  constructor(id: number, command: string) {
+    this.id_ = id;
+    this.command_ = command;
+  }
+
+  /**
+   * Ejecuta el comando.
+   */
+  public abstract execute(): void;
 }
