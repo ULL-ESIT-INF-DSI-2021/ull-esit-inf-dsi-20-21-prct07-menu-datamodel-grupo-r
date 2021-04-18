ac5f94c0b05e4910b9cfa731ba46fe88c5a19c14
Hugo Fernández Solís <alu0101112664@ull.edu.es>
2021-04-18 14:10:14 +0100

    Expectativas de plate cumplidas. Falta predominantGroup.

--- src/Plate.ts
+++ src/Plate.ts
@@ -1,10 +1,11 @@
-import {groupTypes, macroNutrients, nutrientValues, Ingredient} from "./Ingredient";
+import {Ingredient, groupTypes} from './Ingredient';
+import {MacroNutrients} from './MacroNutrients';
 
 
 /**
  * Objeto de tipo TypePlate
  */
-export enum TypePlate {
+export enum typePlate {
   starter = 'Entrante',
   firstPlate = 'Primer plato',
   secondPlate = 'Segundo plato',
@@ -17,7 +18,9 @@ export enum TypePlate {
 export class Plate {
   private name_: string;
   private ingredients_: Ingredient[];
-  // private nutritionalComposition_: nutrientValues;
+  private nutritionalComposition_: MacroNutrients;
+  private predominantGroup_: groupTypes;
+  private price_: number;
 
   /**
    * Constructor de la clase Plate.
@@ -27,40 +30,91 @@ export class Plate {
   constructor(name: string, ingredients: Ingredient[]) {
     this.name_ = name;
     this.ingredients_ = ingredients;
-    // this.nutritionalComposition_ = ();
+    this.nutritionalComposition_ = this.calculateNutritionalValue();
+    this.predominantGroup_ = this.calculatePredominantGroup();
+    this.price_ = this.calculatePrice();
   }
 
   /** ******************************************************************** **/
 
-  public get getName(): string {
+  public get name(): string {
     return this.name_;
   }
 
-  public set setName(newName: string) {
+  public set name(newName: string) {
     this.name_ = newName;
   }
 
   /** ******************************************************************** **/
 
-  public get getIngredients(): Ingredient[] {
+  public get ingredients(): Ingredient[] {
     return this.ingredients_;
   }
 
-  public set setIngredients(newIngredients: Ingredient[]) {
+  public set ingredients(newIngredients: Ingredient[]) {
     this.ingredients_ = newIngredients;
-  }
-
-  public set pushIngredients(newIngredients: Ingredient) {
-    this.ingredients_.push(newIngredients);
+    this.nutritionalComposition_ = this.calculateNutritionalValue();
+    this.predominantGroup_ = this.calculatePredominantGroup();
+    this.price_ = this.calculatePrice();
   }
 
   /** ******************************************************************** **/
 
-  private calculateNutritionalValues(): macroNutrients {
-    let returnValue: macroNutrients;
+  public get nutritionalComposition(): MacroNutrients {
+    return this.nutritionalComposition_;
+  }
+
+  /** ******************************************************************** **/
+
+  public get predominantGroup(): groupTypes {
+    return this.predominantGroup_;
+  }
+
+  /** ******************************************************************** **/
+
+  public get price(): number {
+    return this.price_;
+  }
+
+  public set price(newPrice: number) {
+    this.price_ = newPrice;
+  }
+
+  /** ******************************************************************** **/
+
+  public pushIngredients(newIngredients: Ingredient) {
+    this.ingredients_.push(newIngredients);
+    this.nutritionalComposition_ = this.calculateNutritionalValue();
+    this.predominantGroup_ = this.calculatePredominantGroup();
+    this.price_ = this.calculatePrice();
+  }
+
+  private calculateNutritionalValue(): MacroNutrients {
+    const returnValue = new MacroNutrients(0, 0, 0);
     this.ingredients_.forEach((ingredient) => {
-      returnValue.
+      const carbohydrates = ingredient.nutrients.carbohydrates;
+      const proteins = ingredient.nutrients.proteins;
+      const lipids = ingredient.nutrients.lipids;
+      returnValue.carbohydrates += (carbohydrates * ingredient.amount) / 100;
+      returnValue.proteins += (proteins * ingredient.amount) / 100;
+      returnValue.lipids += (lipids * ingredient.amount) / 100;
     });
     return returnValue;
+  }
+
+  private calculatePredominantGroup(): groupTypes {
+    /* return this.ingredients_.sort((a, b) =>
+      this.ingredients_.filter((v) => v===a).length -
+        this.ingredients_.filter((v) => v===b).length,
+    ).pop().group; */
+    return groupTypes.Fruit;
+  }
+
+  private calculatePrice(): number {
+    let price: number = 0;
+    this.ingredients.forEach((ingredient) => {
+      price += (ingredient.price * ingredient.amount) / 100;
+    });
+    return price;
   }
 };
---
+++ tests/Plate.spec.ts
@@ -0,0 +1,49 @@
+/* eslint-disable max-len */
+import 'mocha';
+import {expect} from 'chai';
+import {Ingredient, groupTypes} from '../src/Ingredient';
+import {MacroNutrients} from '../src/MacroNutrients';
+import {Plate} from '../src/Plate';
+
+const chickenNutrients = new MacroNutrients(60, 40, 20);
+const salmonNutrients = new MacroNutrients(21, 44, 22);
+const orangeNutrients = new MacroNutrients(23, 53, 32);
+const appleNutrients = new MacroNutrients(54, 17, 35);
+const auxNutrient = new MacroNutrients(172, 119.25, 82.25);
+const auxNutrient2 = new MacroNutrients(206.44, 191.41, 118.33);
+
+
+const chicken = new Ingredient("Pollo", 3.86, 200, "El Sauzal", groupTypes.Meat,
+    chickenNutrients);
+const salmon = new Ingredient("Salmón", 7.5, 164, "La Laguna", groupTypes.Fish,
+    salmonNutrients);
+const orange = new Ingredient("Naranja", 1.25, 50, "Taco", groupTypes.Fruit,
+    orangeNutrients);
+const apple = new Ingredient("Manzana", 1.64, 75, "Taco", groupTypes.Fruit,
+    appleNutrients);
+const chickenIngredients: Ingredient[] = [chicken, orange, apple];
+const chickenIngredients2: Ingredient[] = [chicken, orange, apple, salmon];
+
+const firstPlate = new Plate("Pollo con naranja y manzana", chickenIngredients);
+
+
+describe('Funcionamiento básico de la clase Plates.',
+    () => {
+      it('Se puede acceder a sus atributos.', () => {
+        expect(firstPlate.name).to.be.deep.equal("Pollo con naranja y manzana");
+        expect(firstPlate.ingredients).to.be.deep.equal(chickenIngredients);
+        expect(firstPlate.nutritionalComposition).to.be.deep.equal(auxNutrient);
+        expect(firstPlate.predominantGroup).to.be.deep.equal(groupTypes.Fruit);
+        expect(firstPlate.price).to.be.equal(9.575);
+      });
+
+      it('Se pueden modificar sus atributos.', () => {
+        firstPlate.name = "Pollo con naranja, manzana y salmon";
+        firstPlate.pushIngredients(salmon);
+        expect(firstPlate.name).to.be.deep.equal("Pollo con naranja, manzana y salmon");
+        expect(firstPlate.ingredients).to.be.deep.equal(chickenIngredients2);
+        expect(firstPlate.nutritionalComposition).to.be.deep.equal(auxNutrient2);
+        expect(firstPlate.predominantGroup).to.be.deep.equal(groupTypes.Fruit);
+        expect(firstPlate.price).to.be.equal(21.875);
+      });
+    });
