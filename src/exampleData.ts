d94df408141fe54896fdb3f5a9dd2207e905d7fb
Luciano Sekulic Gregoris <alu0101022239@ull.edu.es>
2021-04-18 22:10:34 +0100

    Update exampleData.ts

--- src/exampleData.ts
+++ src/exampleData.ts
@@ -5,34 +5,34 @@ import {MacroNutrients} from './models/MacroNutrients';
 export const rice = new Ingredient("Arroz", 2, 300, "La Laguna",
     groupTypes.Cereal, new MacroNutrients(57, 9, 18.1));
 
-export const chicken = new Ingredient("Pollo", 2, 300, "La Laguna",
+export const chicken = new Ingredient("Pollo", 4, 300, "Tegueste
     groupTypes.Meat, new MacroNutrients(57, 9, 18.1));
 
-export const CowMeat = new Ingredient("Carne", 2, 300, "La Laguna",
+export const CowMeat = new Ingredient("Carne", 4, 300, "La Laguna",
     groupTypes.Meat, new MacroNutrients(57, 9, 18.1));
 
-export const rabbit = new Ingredient("Conejo", 2, 300, "La Laguna",
+export const rabbit = new Ingredient("Conejo", 4, 300, "Tegueste",
     groupTypes.Meat, new MacroNutrients(57, 9, 18.1));
 
-export const quinoa = new Ingredient("quinoa", 2, 300, "La Laguna",
+export const quinoa = new Ingredient("quinoa", 3, 300, "Perú",
     groupTypes.Cereals, new MacroNutrients(57, 9, 18.1));
 
-export const tofu = new Ingredient("Tofu", 2, 300, "La Laguna",
+export const tofu = new Ingredient("Tofu", 2, 300, "Sevilla",
     groupTypes.Cereals, new MacroNutrients(57, 9, 18.1));
 
-export const pasta = new Ingredient("Pasta", 2, 300, "La Laguna",
+export const pasta = new Ingredient("Pasta", 2, 300, "Italia",
     groupTypes.Cereals, new MacroNutrients(57, 9, 18.1));
 
-export const albondigas = new Ingredient("Albondiga", 2, 300, "La Laguna",
+export const albondigas = new Ingredient("Albondiga", 6, 300, "La Laguna",
     groupTypes.Meat, new MacroNutrients(57, 9, 18.1));
 
-export const berenjenas = new Ingredient("Berenjena", 2, 300, "La Laguna",
+export const berenjenas = new Ingredient("Berenjena", 5, 300, "Almeria",
     groupTypes.Vegetables, new MacroNutrients(57, 9, 18.1));
 
-export const calabacin = new Ingredient("Calabacin", 2, 300, "La Laguna",
+export const calabacin = new Ingredient("Calabacin", 2, 300, "Almeria",
     groupTypes.Vegetables, new MacroNutrients(57, 9, 18.1));
 
-export const pescado = new Ingredient("Pescado", 2, 300, "La Laguna",
+export const pescado = new Ingredient("Pescado", 2, 300, "Candelaria",
     groupTypes.Meat, new MacroNutrients(57, 9, 18.1));
 
 
